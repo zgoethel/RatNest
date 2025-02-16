c498dc5ea0c974b81c8a04d33f4819c4be12bdec
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2025-02-15 18:12:19 -0600

    Logic rules and event (un)subscription

---
+++ RatNest/LogicRule.cs
@@ -0,0 +1,34 @@
+﻿namespace RatNest;
+
+public class LogicRule
+{
+    public delegate FormElementState EvaluatorFunc(INamedValue[] selectedValues, FormElementState accumulated);
+
+    public LogicRule(EvaluatorFunc evaluator)
+    {
+        Evaluator = evaluator;
+    }
+
+    public EvaluatorFunc Evaluator { get; private set; }
+
+    public FormElementState Evaluate(FormElementState accumulated)
+    {
+        return Evaluator(SelectedValues, accumulated);
+    }
+
+    public INamedValue[] SelectedValues { get; private set; }
+
+    public event Func<Task> SelectedValuesChanged;
+
+    public async Task InvokeSelectedValuesChanged()
+    {
+        await SelectedValuesChanged.InvokeHandler();
+    }
+
+    public async Task SetSelectedValues(params INamedValue[] values)
+    {
+        SelectedValues = values;
+
+        await InvokeSelectedValuesChanged();
+    }
+}
---
+++ RatNest/LogicRuleSet.cs
@@ -0,0 +1,69 @@
+﻿namespace RatNest;
+
+public class LogicRuleSet
+{
+    private List<INamedValue> prevUniqueValues = new();
+
+    public FormElementState State { get; private set; } = FormElementState.None;
+
+    public event Func<Task> StateChanged;
+
+    private async Task InvokeStateChanged()
+    {
+        await StateChanged.InvokeHandler();
+    }
+
+    private readonly List<LogicRule> rules = new();
+    public IReadOnlyList<LogicRule> Rules => rules;
+
+    public async Task EvaluateRules()
+    {
+        var state = FormElementState.None;
+        foreach (var rule in Rules)
+        {
+            state |= rule.Evaluate(state);
+        }
+
+        if (state != State)
+        {
+            State = state;
+
+            await InvokeStateChanged();
+        }
+    }
+
+    private async Task UpdateSubscriptions()
+    {
+        var currUniqueValues = Rules
+            .SelectMany((it) => it.SelectedValues)
+            .DistinctBy((it) => it.RefId)
+            .ToList();
+
+        var unsubscribe = prevUniqueValues.Where((it) => !currUniqueValues.Any((_it) => it.RefId == it.RefId));
+        var subscribe = currUniqueValues.Where((it) => !prevUniqueValues.Any((_it) => it.RefId == it.RefId));
+
+        foreach (var value in unsubscribe)
+        {
+            value.ValueChanged -= EvaluateRules;
+        }
+
+        foreach (var value in subscribe)
+        {
+            value.ValueChanged += EvaluateRules;
+        }
+
+        prevUniqueValues = currUniqueValues;
+
+        await EvaluateRules();
+    }
+
+    public LogicRule CreateLogicRule(LogicRule.EvaluatorFunc evaluator)
+    {
+        var rule = new LogicRule(evaluator);
+        rule.SelectedValuesChanged += UpdateSubscriptions;
+
+        rules.Add(rule);
+
+        return rule;
+    }
+}
